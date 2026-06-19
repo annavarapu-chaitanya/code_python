b48c233e9975bfce313ae7325ea282d6696aaba9
Chaitanya <chaitanya12603@gmail.com>
2026-06-19 14:21:39 +0530

    Add files via upload

---
+++ range/ran1.py
@@ -0,0 +1,6 @@
+n = 4
+
+for i in range(n):
+    for j in range(n, 0, -1):
+        print(j, end=" ")
+    print()
---
+++ range/range.py
@@ -0,0 +1,5 @@
+l= ["a","b","c","d"]
+length=len(l)
+
+for i in range(length-1,-1,-1):
+    print(i,l[i].upper())
